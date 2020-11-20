6e3dd973addc51ed4b1c8dd824ddb9de934e2721
Hidenori Matsuki <dev@mazgi.com>
2020-11-20 13:43:51 +0000

    update terraform version

--- provisioning/examples/terraform.tf
+++ provisioning/examples/terraform.tf
@@ -2,12 +2,12 @@
 # Terraform configuration
 
 terraform {
-  required_version = "0.13.4"
+  required_version = "0.13.5"
 
   required_providers {
-    aws         = "3.9.0"
-    google      = "3.42.0"
-    google-beta = "3.42.0"
+    aws         = "3.16.0"
+    google      = "3.48.0"
+    google-beta = "3.48.0"
   }
 
   backend "gcs" {
