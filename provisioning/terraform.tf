af4b296ede9c482466743de74f42778852b21d09
Hidenori Matsuki <dev@mazgi.com>
2022-10-13 23:43:54 +0900

    specify terraform and providers versions to be a bit looser.

--- provisioning/terraform.tf
+++ provisioning/terraform.tf
@@ -1,27 +1,32 @@
 terraform {
   # https://www.terraform.io/downloads.html
-  required_version = "1.2.8"
+  # See https://www.terraform.io/language/expressions/version-constraints#version-constraint-syntax
+  #  = (or no operator): Allows only one exact version number. Cannot be combined with other conditions.
+  #  !=: Excludes an exact version number.
+  #  >, >=, <, <=: Comparisons against a specified version, allowing versions for which the comparison is true. "Greater-than" requests newer versions, and "less-than" requests older versions.
+  #  ~>: Allows only the rightmost version component to increment. For example, to allow new patch releases within a specific minor release, use the full version number: ~> 1.0.4 will allow installation of 1.0.5 and 1.0.10 but not 1.1.0. This is usually called the pessimistic constraint operator.
+  required_version = "~> 1.2.0"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "=4.27.0"
+      version = "~> 4.27.0"
     }
     # https://registry.terraform.io/providers/hashicorp/azurerm/latest
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.19.1"
+      version = "~> 3.19.0"
     }
     # https://registry.terraform.io/providers/hashicorp/google/latest
     google = {
       source  = "hashicorp/google"
-      version = "=4.33.0"
+      version = "~> 4.33.0"
     }
     # https://registry.terraform.io/providers/hashicorp/google-beta/latest
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "=4.33.0"
+      version = "~> 4.33.0"
     }
   }
 }
