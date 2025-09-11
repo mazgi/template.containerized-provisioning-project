94a33973ff6431fb1dd30f20d04cdd60fd527b74
Hidenori "Hide" Matsuki <dev@mazgi.com>
2025-09-11 20:34:57 +0900

    fix aws rds samples

--- provisioning/examples/aws.rds.tf
+++ provisioning/examples/aws.rds.tf
@@ -1,46 +1,65 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = tolist(aws_subnet.private.*.id)
+}
+
+resource "aws_db_parameter_group" "custom" {
+  name        = "custom-mysql8"
+  family      = "mysql8.0"
+  description = "MySQL 8.0 parameter group"
+
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "long_query_time"
+    value = "0"
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+}
+
 resource "random_string" "db-password" {
   length  = 8
   special = false
 }
 
 resource "aws_db_instance" "main" {
-  allocated_storage       = 10
-  db_name                 = "app_rds"
-  db_subnet_group_name    = aws_db_subnet_group.main.name
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  identifier              = "main"
-  instance_class          = "db.t3.micro"
-  username                = "admin"
-  password                = random_string.db-password.result
-  parameter_group_name    = "default.mysql8.0"
-  skip_final_snapshot     = true
-  backup_retention_period = 1
-  enabled_cloudwatch_logs_exports = [
-    "error",
-    "general",
-    "slowquery"
-  ]
+  allocated_storage               = 10
+  backup_retention_period         = 1
+  db_name                         = "app_rds"
+  db_subnet_group_name            = aws_db_subnet_group.main.name
+  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  engine                          = "mysql"
+  engine_version                  = "8.0"
+  identifier                      = "main"
+  instance_class                  = "db.t3.micro"
+  password                        = random_string.db-password.result
+  parameter_group_name            = aws_db_parameter_group.custom.name
+  skip_final_snapshot             = true
+  username                        = "admin"
   vpc_security_group_ids = [
     aws_security_group.allow-any-from-vpc.id,
   ]
 }
 
-resource "aws_db_subnet_group" "main" {
-  name       = "main"
-  subnet_ids = tolist(aws_subnet.private.*.id)
-}
-
 resource "aws_db_instance" "replica" {
-  replicate_source_db = aws_db_instance.main.arn
-  identifier          = "replica"
-  instance_class      = "db.t3.micro"
-  skip_final_snapshot = true
-  enabled_cloudwatch_logs_exports = [
-    "error",
-    "general",
-    "slowquery"
-  ]
+  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  identifier                      = "replica"
+  instance_class                  = "db.t3.micro"
+  parameter_group_name            = aws_db_parameter_group.custom.name
+  replicate_source_db             = aws_db_instance.main.arn
+  skip_final_snapshot             = true
   vpc_security_group_ids = [
     aws_security_group.allow-any-from-vpc.id,
   ]
